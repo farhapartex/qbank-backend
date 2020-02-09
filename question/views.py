394f509c40d9c1c9b73ec4ced6b420491a630051
Md Nazmul Hasan <farhapartex@gmail.com>
2020-02-10 04:18:46 +0600

    question list filter added

--- question/views.py
+++ question/views.py
@@ -1,4 +1,4 @@
-import logging
+import logging, datetime
 from django.shortcuts import render, get_object_or_404
 from django_filters import rest_framework as filters
 from rest_framework import generics, viewsets
@@ -30,6 +30,49 @@ class CourseFilter(filters.FilterSet):
         fields = ("id",)
 
 
+class QuestionFilter(filters.FilterSet):
+    year = filters.CharFilter(method="filter_by_exam_year")
+    department = filters.CharFilter(method="filter_by_department")
+    semester = filters.CharFilter(method="filter_by_semester")
+    course = filters.CharFilter(method="filter_by_course")
+
+    def filter_by_exam_year(self, queryset, name, value):
+        if value is None:
+            return queryset
+        try:
+            return queryset.filter(year=value)
+        except:
+            return queryset.none()
+    
+    def filter_by_department(self, queryset, name, value):
+        if value is None:
+            return queryset
+        try:
+            return queryset.filter(department__id=value)
+        except:
+            return queryset.none()
+
+    def filter_by_semester(self, queryset, name, value):
+        if value is None:
+            return queryset
+        try:
+            return queryset.filter(semester=value)
+        except:
+            return queryset.none()
+    
+    def filter_by_course(self, queryset, name, value):
+        if value is None:
+            return queryset
+        try:
+            return queryset.filter(course__id=value)
+        except:
+            return queryset.none()
+
+    class Meta:
+        model = Question
+        fields = ("year","department","semester","course")
+
+
 """
 API Views
 """
@@ -49,15 +92,10 @@ class QuestionAPIViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = QuestionFilter
 
     def get_serializer_class(self):
         return QuestionDetailSerializer if self.action == "list" else QuestionSerializer
-
-
-class QuestionReadOnlyAPIViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def get_serializer_class(self):
-        return QuestionDetailSerializer if self.action == "list" else QuestionSerializer
+    
+    
